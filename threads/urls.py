9517122a6a8e1edcf80e86a1cbf062933676611b
mohamadsaad1 <mohamad_utd@outlook.com>
2021-11-29 11:51:00 -0600

    create ui

--- threads/urls.py
+++ threads/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  # localhost:8000/
     path('', views.ThreadList.as_view(), name='home'),
-    path('<slug:slug>/', views.ThreadDetail.as_view(), name='thread_detail'),
-    
+
+    path('<slug:slug>/', views.ThreadDetail.as_view(), 
+    name='thread_detail'), 
+
+    path('create/', views.ThreadCreate.as_view(), name='threads_create'),
 ]
--- threads/views.py
+++ threads/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic.edit import CreateView
 from .models import Thread
 
+
+
+def threads_index(request):
+  threads = Thread.objects.all()
+  return render(request, 'threads/index.html', { 'threads': threads })
 
 class ThreadList(generic.ListView):
     queryset = Thread.objects.filter(status=1).order_by('-created_on')
@@ -10,3 +16,7 @@ class ThreadList(generic.ListView):
 class ThreadDetail(generic.DetailView):
     model = Thread
     template_name = 'thread_detail.html'
+
+class ThreadCreate(CreateView):
+  model = Thread
+  fields = '__all__'
