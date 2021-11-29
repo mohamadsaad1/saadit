5cd5919f7b5347fab9f71691e0f4f45eebf0266f
mohamadsaad1 <mohamad_utd@outlook.com>
2021-11-29 10:26:11 -0600

    create admin and thread in admin panel

--- saadit/urls.py
+++ saadit/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # In this case '' represents the root route
-    path('', include('main_app.urls')),
+    path('', include('threads.urls')),
 ]
--- threads/views.py
+++ threads/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello</h1>')
+  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
